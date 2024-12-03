c0dc1287586fb05ff496cd9d58159160f285ae7d
bstachowski <bstachowski@oferteo.pl>
2024-12-03 16:56:59 +0100

    test

---
+++ src/components/__tests__/AppSelect.spec.js
@@ -0,0 +1,19 @@
+import { describe, it, expect } from 'vitest'
+
+import { mount } from '@vue/test-utils'
+import AppSelect from '../AppSelect.vue'
+
+
+
+describe('AppSelect Component', () => {
+    it('renders properly', () => {
+        const options = ['Option 1', 'Option 2'];
+        const wrapper = mount(AppSelect, {props: {label: 'Select', options}});
+        
+        console.log(wrapper.html())
+        const optionsEl = wrapper.findAllComponents('option');
+        
+        expect(wrapper.text()).toContain('Option 2')
+        expect(wrapper.text()).not.toContain('Option 3')
+    })
+})
