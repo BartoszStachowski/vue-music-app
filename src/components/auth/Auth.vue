d3fa5969e7d1da3e3b19a8f1a1733cb1fe0e6860
BartoszStachowski <stachowskibartosz96@gmail.com>
2024-12-20 12:11:45 +0100

    format

--- src/components/auth/Auth.vue
+++ src/components/auth/Auth.vue
@@ -1,37 +1,46 @@
 <template>
   <AppModal @close="closeModal">
-    <ul class="flex flex-wrap mb-4">
+    <ul class="mb-4 flex flex-wrap">
       <li class="flex-auto text-center">
-        <a class="block px-4 py-3 transition rounded" href="#" :class="{
-          'bg-blue-600 text-white hover:text-white': tab === 'login',
-          'hover:text-blue-600': tab === 'register',
-        }" @click.prevent="tab = 'login'">
+        <a
+          class="block rounded px-4 py-3 transition"
+          href="#"
+          :class="{
+            'bg-blue-600 text-white hover:text-white': tab === 'login',
+            'hover:text-blue-600': tab === 'register',
+          }"
+          @click.prevent="tab = 'login'"
+        >
           Login
         </a>
       </li>
       <li class="flex-auto text-center">
-        <a class="block px-4 py-3 transition rounded" href="#" :class="{
-          'bg-blue-600 text-white hover:text-white': tab === 'register',
-          'hover:text-blue-600': tab === 'login',
-        }" @click.prevent="tab = 'register'">
+        <a
+          class="block rounded px-4 py-3 transition"
+          href="#"
+          :class="{
+            'bg-blue-600 text-white hover:text-white': tab === 'register',
+            'hover:text-blue-600': tab === 'login',
+          }"
+          @click.prevent="tab = 'register'"
+        >
           Register
         </a>
       </li>
     </ul>
 
     <!-- Login Form -->
-    <Login v-show="tab === 'login'" />
+    <LoginForm v-show="tab === 'login'" />
 
     <!-- Registration Form -->
-    <Register v-show="tab === 'register'" />
-
+    <RegisterForm v-show="tab === 'register'" />
   </AppModal>
 </template>
 <script setup>
 import useModalStore from '@/stores/modal';
 import AppModal from '@/components/AppModal.vue';
-import Login from '@/components/auth/Login.vue'
-import Register from '@/components/auth/Register.vue'
+import LoginForm from '@/components/auth/LoginForm.vue';
+import RegisterForm from '@/components/auth/RegisterForm.vue';
 import { ref } from 'vue';
 
 // store
@@ -41,6 +50,5 @@ const tab = ref('login');
 
 const closeModal = () => {
   modalStore.close('authModal');
-}
-
+};
 </script>
